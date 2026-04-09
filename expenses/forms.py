3bf7211e76b735da6be1cb6acaf5662e6db071b1
Madhura Ravishan Abeywickrama <madhuraravishan@gmail.com>
2026-04-09 18:25:20 +0530

    Integrated calendar, default date, and smart category suggestions

--- expenses/forms.py
+++ expenses/forms.py
@@ -4,3 +4,8 @@ class ExpenseForm(forms.ModelForm):
     class Meta:
         model=Expense
         fields=['title','amount','date', 'category']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'category': forms.TextInput(attrs={'list': 'category-list', 'placeholder': 'Select or type new...'})
+            }
+        
--- expenses/models.py
+++ expenses/models.py
@@ -1,15 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User  # <-- 1. Add this import at the top
+from django.contrib.auth.models import User  
+from django.utils import timezone
 
 class Expense(models.Model):
-    # <-- 2. ADD THIS NEW LINE -->
-    # (default=1 means all your old expenses will be assigned to your admin account)
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1) 
     
     title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField()
+    date = models.DateField(default=timezone.now)
     category = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.title} - {self.amount}"
+    
+    def __clstr__(self):
+        return self.title
